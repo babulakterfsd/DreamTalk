04246e7a379b1716b699e049a26fb527ac8a7de6
Babul Akter <babulakterfsd@gmail.com>
2023-07-08 11:23:43 +0600

    possibilities texts changed with animation

--- src/components/ui/Possibilities.tsx
+++ src/components/ui/Possibilities.tsx
@@ -1,11 +1,8 @@
-import Image from 'next/image';
-import possibility from '../../../public/assets/images/Arrow 1.png';
-
 const Possibilities = () => {
   return (
     <section className="bg-[#040d19] pt-20 pb-16 lg:py-28 xl:py-20 relative">
       <div className="main-container">
-        <div
+        {/* <div
           className="lg:flex justify-center lg:justify-start items-center"
           data-aos="fade-down"
           data-aos-duration="500"
@@ -23,6 +20,24 @@ const Possibilities = () => {
           <h1 className="font-crimson lg:text-6xl lg:leading-[64px] lg:ml-20  lg:mt-28 font-semibold hidden lg:block capitalize">
             {' '}
             your <br /> <span className="lg:-ml-40">imagination</span>
+          </h1>
+        </div> */}
+        <div className="lg:flex justify-center items-center">
+          <h1 className="font-crimson text-2xl leading-[32px] lg:text-6xl lg:leading-[64px] font-bold lg:font-semibold text-center lg:px-0 capitalize lg:-ml-10">
+            <span
+              className="block"
+              data-aos="fade-down"
+              data-aos-duration="1500"
+            >
+              The only limit is <br className=" hidden lg:block" />{' '}
+            </span>
+            <span
+              className="block"
+              data-aos="fade-down"
+              data-aos-duration="2000"
+            >
+              your imagination
+            </span>
           </h1>
         </div>
       </div>
